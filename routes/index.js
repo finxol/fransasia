6ae13b73a2937b2dbf1b4fefb42f86e16acaac8d
finxol <personnal@bottleneck.anonaddy.com>
2022-01-06 00:16:58 +0100

    add `favicon.ico` route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
@@ -6,6 +7,10 @@ router.get('/', (req, res, next) => {
     res.render('index', {
         title: 'index'
     });
+});
+
+router.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "images", "favicon", "favicon.ico"))
 });
 
 module.exports = router;
