9f8e415bfe7d0311dcc817ef5e222ec2b43e1568
finxol <personnal@bottleneck.anonaddy.com>
2022-01-09 19:09:24 +0100

    move to ES6 + register `isCurrentPage` helper + dynamic year

--- app.js
+++ app.js
@@ -1,51 +1,68 @@
-const createError = require('http-errors');
-const express = require('express');
-const hbs = require('express-hbs');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
+import createError from 'http-errors';
+import express from 'express';
+import hbs from 'express-hbs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import router from './routes/index.js';
+import mobile from './public/js/mobile.js';
+import fs from "fs";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const relative = p => path.join(__dirname, p);
 
 // view engine setup
 const viewsDir = relative('views');
-
 app.use(express.static(relative('public')));
 
 // Hook in express-hbs and tell it where known directories reside
 app.engine('hbs', hbs.express4({
-  layoutsDir: relative('views/layout'),
-  defaultLayout: relative('views/layout.hbs')
+    layoutsDir: relative('views/layout'),
+    defaultLayout: relative('views/layout.hbs'),
 }));
 app.set('view engine', 'hbs');
 app.set('views', viewsDir);
 
-app.use(logger('dev'));
+hbs.registerHelper('isCurrentPage', (a, b, options) => {
+    if (a === b) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
+
+
+let format = app.get('env') === "developement" ? 'dev' : 'tiny';
+app.use(logger(format));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.locals.year = new Date().getFullYear();
+
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', {
+        title: 'index',
+        mobile: !!mobile(req.headers['user-agent'])
+    });
 });
 
-module.exports = app;
+export default app;
