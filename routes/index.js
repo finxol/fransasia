3beb9dea80fade321c18d3d38bf7400c46600fdf
finxol <personnal@bottleneck.anonaddy.com>
2022-01-09 19:16:49 +0100

    move to ES6 + add about page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,30 @@
-const path = require('path');
-const express = require('express');
+import path from 'path';
+import mobile from '../public/js/mobile.js';
+import express from 'express';
+import {fileURLToPath} from "url";
 const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', {
-        title: 'index'
+        title: 'index',
+        mobile: !!mobile(req.headers['user-agent'])
+    });
+});
+
+/* GET home page. */
+router.get('/about', (req, res, next) => {
+    res.render('about', {
+        title: 'about',
+        mobile: !!mobile(req.headers['user-agent'])
     });
 });
 
 router.get('/favicon.ico', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "images", "favicon", "favicon.ico"))
+    res.sendFile(path.join(__dirname, "..", "public", "images", "favicon", "favicon.ico"))
 });
 
-module.exports = router;
+export default router;
