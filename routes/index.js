72c071b4e3e130bbb416fd6986118e1ca88bd7ca
finxol <personnal@bottleneck.anonaddy.com>
2022-02-27 00:29:00 +0100

    create pages

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/* GET favicon */
+router.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "images", "favicon", "favicon.ico"))
+});
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', {
@@ -15,16 +20,14 @@ router.get('/', (req, res, next) => {
     });
 });
 
-/* GET home page. */
-router.get('/about', (req, res, next) => {
-    res.render('about', {
-        title: 'about',
+/* GET about page. */
+router.get(/^\/(about|partner-deal|manage-deal|convergence|green|macau)$/, (req, res, next) => {
+    let page = req.path.substring(1);
+    res.render(page, {
+        title: page,
         mobile: !!mobile(req.headers['user-agent'])
     });
 });
 
-router.get('/favicon.ico', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public", "images", "favicon", "favicon.ico"))
-});
 
 export default router;
