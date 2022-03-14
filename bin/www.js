425f47f345464cf89cac2bd6621c55f3353db7d1
finxol <personnal@bottleneck.anonaddy.com>
2022-03-14 20:44:04 +0100

    change port

--- bin/www.js
+++ bin/www.js
@@ -13,7 +13,7 @@ const debug = Debug("fransasia:server");
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
 
 /**
